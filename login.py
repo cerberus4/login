b266650359b11df24e579eb6c2d93ebd1e3d1b70
cerberus4 <80750769+cerberus4@users.noreply.github.com>
2021-08-29 18:32:03 +1000

    Add files via upload

--- login.py
+++ login.py
@@ -1,20 +1,54 @@
-
 import getpass
+import sqlite3
+import sys
+
+def menu():
+    print('Enter Number to continue:\n--------------------------\n1. Login \n2. Register \n3. Quit')
+    entry = input('')
+    if entry == '1':
+        login()
+    elif entry == '2':
+        print('register')
+    elif entry == '3':
+        print('quitting application')
+        sys.exit
+    else:
+        print('error')
+        menu()
+
 def login():
-    print('Enter Number to continue:\n--------------------------\n1. Login \n2. Register')
-    entry = input
-    username = input('Username:')
-
+    
+    usernameinput = input('Username:')
+    username = usernameinput,
     password = getpass.getpass()
+    
+    con = sqlite3.connect('logindb.db')
+    cur = con.cursor()
+    cur.execute('SELECT password from USERS WHERE username = ?', username)
+    pwd = cur.fetchone()
+    if pwd[0] == password:
+        print('welcome')
+    else:
+        print('Returning you to menu')
+        menu()
 
-    print(username)
-    print(password)
+def register():
+    print('Enter desired username')
+    #enter username
+    #check if free
+    #enter password
+    #count total names in user table
+    #add 1 to the total and use as primary key
+    
 
-#Form Tutor Management System
-import sys #this allows you to use the sys.exit command to quit/logout of the application
-def main():
-    login()
 
 #test change
 #the program is initiated, so to speak, here
-main()
+menu()
+
+'''
+CREATE TABLE IF NOT EXISTS Login(
+    Username TEXT,
+    Password TEXT
+);
+'''
